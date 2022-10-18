484405417518a2328817c9526ead153cec5999cd
JadenTags <jadentagulinao@yahoo.com>
2022-10-18 09:59:43 -0700

    Removed gitIgnore

---
+++ Scripts/config.js
@@ -0,0 +1,12 @@
+var config = {
+  tbaKey: 'SbjZMqawBlMTp7aRoJC5V73h7e4CBbQ2mbF8xJssv8nql1I0fCy8lX8O05qP0zqt',
+  clientId: '1084130048210-7fog59udh3li01r1f3pibrceli874mc1.apps.googleusercontent.com',
+  clientSecret: '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',
+  refreshToken: '1//04EXXe5i5LTVaCgYIARAAGAQSNwF-L9Ir7J-LRbvXAE3xG98RuoSJeohVi9RDRpBxT504J2Mw7EaN8xF7_GBCFmqtl8JWxZCEbYo',
+  userDatabaseGSID: '1mzkuHHLonKO10spvtKyDiqLP9OBwvpH9kr--1rlDA2I',
+  passcodeGSID: '1PykseSbyrWuSDvToeotIroMgG4QZ2kGB3KkP4MtyunA',
+  feedbackGSID: '1dRcoweLtoj_3WwkfyTcLZeeM1eXbYzOqL3v5SDx6U_M',
+  matchGSID: '1EyS5XVJtnQsq4N8wv6kC91DbrzBDKiQIqCjVwXtQxDA',
+  pitGSID: '17nIqUwNFmR7yCaeMNNzv3oP95C0B_nyGOfT1EaeAPMQ',
+  preGSID: '11gDk29ZE7jqMtRu-JJ2X3pA_gdDqadYCEc2C7o4-SWo',
+}
